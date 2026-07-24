4a4688fb115335dcd0b88e7acfba3a324ea20c97
Matthew Barker <matthewbarker@librem.one>
2026-07-24 16:18:19 +0100

    Add back cba_xeh dependency for CfgEventHandlers auto-init
    CfgEventHandlers requires cba_xeh to process init functions. Without it, a3db_fnc_init is never called, extension never loads. docs: Updated CBA dependency table.

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches {
         name = "A3DB - Main";
         author = "ABE Team";
         requiredVersion = 2.02;
-        requiredAddons[] = {"cba_main"};
+        requiredAddons[] = {"cba_main", "cba_xeh"};
         units[] = {};
         weapons[] = {};
         forceRenameLib = "a3db";
