de67f5c096be167722c6b790ad5026536d08da39
Matthew Barker <matthewbarker@librem.one>
2026-07-28 18:40:30 +0100

    fix: CfgEventHandlers compileScript paths — CBA settings now load correctly

--- addons/database/CfgEventHandlers.hpp
+++ addons/database/CfgEventHandlers.hpp
@@ -1,10 +1,10 @@
 class Extended_PreInit_EventHandlers {
     class ADDON {
-        init = "call a3sql_database_fnc_preInit";
+        init = "call compileScript ['z\a3sql\addons\database\XEH_preInit.sqf']";
     };
 };
 class Extended_PostInit_EventHandlers {
     class ADDON {
-        init = "call a3sql_database_fnc_postInit";
+        init = "call compileScript ['z\a3sql\addons\database\XEH_postInit.sqf']";
     };
 };
