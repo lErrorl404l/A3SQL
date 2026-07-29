849761f6e2e5fb3fd6b5589baaaaf6a7476ca182
Matthew Barker <matthewbarker@librem.one>
2026-07-29 22:13:14 +0100

    feat: add DISABLE_COMPILE_CACHE support to A3PREP macro

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -14,5 +14,9 @@
 #define A3DEFUNC(var1,var2) TRIPLES(DOUBLES(PREFIX,var1),fnc,var2)
 #define A3QFUNC(var1) QUOTE(A3FUNC(var1))
 #define A3QEFUNC(var1,var2) QUOTE(A3DEFUNC(var1,var2))
+
+#ifdef DISABLE_COMPILE_CACHE
+#define A3PREP(fncName) A3FUNC(fncName) = compile preprocessFileLineNumbers QPATHTOF(functions\DOUBLES(fnc,fncName).sqf)
+#else
 #define A3PREP(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName).sqf), A3QFUNC(fncName)] call CBA_fnc_compileFunction
-#define A3VERSION_CONFIG version = VERSION; versionStr = QUOTE(VERSION); versionAr[] = {VERSION_MAJOR,VERSION_MINOR,VERSION_PATCH}
+#endif
